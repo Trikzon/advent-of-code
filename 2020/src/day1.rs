31b707ca361dcd43288bedb7febb8b1cdd3a27aa
Trikzon <trikzondev@gmail.com>
2020-12-01 18:28:26 -0500

    Generify day outputs

--- 2020/src/day1.rs
+++ 2020/src/day1.rs
@@ -2,30 +2,32 @@ const INPUT: &str = include_str!("day1.input");
 
 pub struct Day1;
 impl crate::Day for Day1 {
-    fn part1() -> Option<i32> {
+    type Output = i32;
+
+    fn part1() -> Self::Output {
         let input = parse_input();
 
         for i in &input {
             let required_addend = 2020 - i;
             if input.contains(&required_addend) {
-                return Some(i * required_addend);
+                return i * required_addend;
             }
         }
-        None
+        unreachable!();
     }
 
-    fn part2() -> Option<i32> {
+    fn part2() -> Self::Output {
         let input = parse_input();
 
         for x in &input {
             for y in &input {
                 let required_addend = 2020 - x - y;
                 if input.contains(&required_addend) {
-                    return Some(x * y * required_addend);
+                    return x * y * required_addend;
                 }
             }
         }
-        None
+        unreachable!();
     }
 }
 
