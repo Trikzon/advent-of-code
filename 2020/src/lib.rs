b152f46bfb89657c4c4e436f82cd55b0b1918418
Trikzon <trikzondev@gmail.com>
2020-12-01 18:28:26 -0500

    Generify day outputs

--- 2020/src/lib.rs
+++ 2020/src/lib.rs
@@ -3,16 +3,18 @@
 mod day1;
 
 pub trait Day {
-    fn part1() -> Option<i32>;
-    fn part2() -> Option<i32>;
+    type Output: std::fmt::Debug + PartialEq;
+
+    fn part1() -> Self::Output;
+    fn part2() -> Self::Output;
 }
 
 #[test]
 fn day_1_1() {
-    assert_eq!(1019904, day1::Day1::part1().unwrap());
+    assert_eq!(1019904, day1::Day1::part1());
 }
 
 #[test]
 fn day_1_2() {
-    assert_eq!(176647680, day1::Day1::part2().unwrap());
+    assert_eq!(176647680, day1::Day1::part2());
 }
