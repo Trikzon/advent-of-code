56540938ecc22f90d256bb8f69a4acf4453b3fd1
Trikzon <trikzondev@gmail.com>
2020-12-02 14:07:21 -0500

    Add criterion benchmarks

---
+++ 2020/benches/benchmarks.rs
@@ -0,0 +1,12 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use aoc_2020::*;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("day_1_1", |b| b.iter(|| day1::Day1::part1()));
+    c.bench_function("day_1_2", |b| b.iter(|| day1::Day1::part2()));
+    c.bench_function("day_2_1", |b| b.iter(|| day2::Day2::part1()));
+    c.bench_function("day_2_2", |b| b.iter(|| day2::Day2::part2()));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
