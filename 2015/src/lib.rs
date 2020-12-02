ce95af0c9d9f9a7d42c617e3cc9f6c0d44995e6a
Trikzon <trikzondev@gmail.com>
2020-12-01 19:22:40 -0500

    2015-day2

--- 2015/src/lib.rs
+++ 2015/src/lib.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 
 mod day1;
+mod day2;
 
 pub trait Day {
     type Output: std::fmt::Debug + PartialEq;
@@ -17,4 +18,14 @@ fn day_1_1() {
 #[test]
 fn day_1_2() {
     assert_eq!(1771, day1::Day1::part2());
+}
+
+#[test]
+fn day_2_1() {
+    assert_eq!(1606483, day2::Day2::part1());
+}
+
+#[test]
+fn day_2_2() {
+    assert_eq!(3842356, day2::Day2::part2());
 }
